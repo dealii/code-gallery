6426536343cf8c9714dec6cc36bf61ebe5c4a62c
Salvador Flores <localadm@localadm-desktop.(none)>
2015-10-16 15:56:14 -0300

    .

--- Elastoplastic Torsion/CMakeFiles/help.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/help"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/help.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
