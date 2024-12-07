2351ea5e49e7e14c1580cd9268ee353c1afadf14
Wasim Niyaz Munshi <munshiw0@gmail.com>
2024-12-06 17:46:04 -0700

    Changed namespace from step854 to PhaseField

--- Phase_field_fracture_model_in_3D/phase_field.cc
+++ Phase_field_fracture_model_in_3D/phase_field.cc
@@ -48,7 +48,7 @@ using namespace dealii::LinearAlgebraTrilinos;
 #include <iostream>
 #include <random>
 
-namespace Step854
+namespace StepPhaseField
 
 {
   using namespace dealii;
@@ -1437,7 +1437,7 @@ main (int argc,
   try
     {
       using namespace dealii;
-      using namespace Step854;
+      using namespace StepPhaseField;
 
       Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
 
